0bb5a6debf432baf2a9c4c4f596bb45f05275f20
Alex Balabanov <alxndr.blbnv@gmail.com>
2022-02-12 21:30:48 +0200

    Add logger file

---
+++ StreetFoodTrackerPackage/Sources/LoggingService/LoggingService.swift
@@ -0,0 +1,14 @@
+//
+//  LoggingService.swift
+//  
+//
+//  Created by Oleksandr Balabanov on 12.02.2022.
+//
+
+import Foundation
+
+public final class LoggingService {
+    public static func log(_ message: String) {
+        os_log("something")
+    }
+}
